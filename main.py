60e3acee39953f414a39e2bc93df8a0af139f4d5
nuke <rochadcmarcos@gmail.com>
2019-05-05 04:25:41 -0300

    tkinter principal interface

--- main.py
+++ main.py
@@ -1,30 +1,64 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from api_ import GetBitcoin, GetDollar, GetEuro, GetPeso
+from tkinter import *
+from api import *
 
-class Get_image (BoxLayout):
+class Packing:
+    def __init__(self,tk_instance):
+        #containers
+        self.container1 = Frame(tk_instance)
+        self.container2 = Frame(tk_instance)
+        self.container3 = Frame(tk_instance)
+        
+        #packing
+        self.container1.pack()
+        self.container2.pack()
+        self.container3.pack()
 
-    def GetDollarValue(self):
-        return "R$ %.2f" %GetDollar()
-    def GetDollarVariation(self):
-        return GetDollar('variation')
-    def GetPesoValue(self):
-        return "R$ %.2f" %GetPeso() 
-    def GetPesoVariation(self):
-        return GetPeso('variation')
-    def GetBitcoinValue(self):
-        return "R$ %.2f" %GetBitcoin()
-    def GetBitcoinVariation(self):
-        return GetBitcoin('variation')
-    def GetEuroValue(self):
-        return "R$ %.2f" %GetEuro()
-    def GetEuroVariation(self):
-        return GetEuro("variation")
+        #variables value
+        self.dollar_value = f'R$ {GetDollar()}'
+        self.dollar_variation = GetDollar('variation')
+        self.euro_value = f'R$ {GetEuro()}'
+        self.euro_variation = GetEuro('variation')
+        self.peso_value = f'R$ {GetPeso()}'
+        self.peso_variation = GetPeso('variation')
+        self.bit_value = f'R$ {GetBitcoin()}'
+        self.bit_variation = GetBitcoin('variation')
 
-class Interface(App):
+        #labels and buttons
+        Label(self.container1, text='Cotações').pack(side=TOP)
+        #Dollar
+        Label(self.container2, text='Dollar').pack()
+        Label(self.container2, text=self.dollar_value).pack()
+        Label(self.container2, text=self.dollar_variation).pack()
+        #Euro
+        Label(self.container2, text='Euro').pack()
+        Label(self.container2, text=self.euro_value).pack()
+        Label(self.container2, text=self.euro_variation).pack()
+        #Peso
+        Label(self.container2, text='Peso').pack()
+        Label(self.container2, text=self.peso_value).pack()
+        Label(self.container2, text=self.peso_variation).pack()
+        #Bitcoin
+        Label(self.container2, text='Bitcoin')
+        Label(self.container2, text=self.bit_value).pack()
+        Label(self.container2, text=self.bit_variation).pack()
+        #Reload bitcoins
+        self.button_reload = Button(self.container3, text='Reload Coins')
+        self.button_reload.pack(side=LEFT)
+        Button(self.container3, text='Plot Graphic').pack(side=LEFT)
+        Button(self.container3, text='Exit').pack(side=LEFT)
 
-    def build(self):
+    def reload_api():
+        self.dollar_value = f'R$ {GetDollar()}'
+        self.dollar_variation = GetDollar('variation')
+        self.euro_value = f'R$ {GetEuro()}'
+        self.euro_variation = GetEuro('variation')
+        self.peso_value = f'R$ {GetPeso()}'
+        self.peso_variation = GetPeso('variation')
+        self.bit_value = f'R$ {GetBitcoin()}'
+        self.bit_variation = GetBitcoin('variation')
+        print(self.dollar_value, self.euro_value)
 
-        return Get_image()
-
-Interface().run()
+if __name__ == '__main__':
+    raiz = Tk()
+    Packing(raiz)
+    raiz.mainloop()
